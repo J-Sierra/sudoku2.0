8d28d4615813c2baa32bd5e0c4c0d873407e5ad5
Johnny <gjohnnys@att.net>
2023-10-10 00:11:36 -0700

    Subdomain Sudoku.johnnysierra.com setup

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sudoku/",
+  base: "https://sudoku.johnnysierra.com/",
   plugins: [react()],
   build: {
     outDir: "docs",

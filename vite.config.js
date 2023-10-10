3ff654d29ecd6c85628476a1cd1b7e4bb412d535
Johnny <gjohnnys@att.net>
2023-10-09 21:12:15 -0700

    Sudoku submodule built

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sudoku/',
+  base: "/sudoku/",
   plugins: [react()],
-})
+  build: {
+    outDir: "docs",
+  },
+});
